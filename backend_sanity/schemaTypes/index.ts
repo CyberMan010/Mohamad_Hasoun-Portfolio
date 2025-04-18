99fd08836c0b53a4a6ff5322d9158ca423017f0a
CyberMan010 <mohamahasoun60@gmail.com>
2025-04-18 22:41:53 +0300

    backend done

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -1,2 +1,10 @@
 import testimonials from "./testimonials"
-export const schemaTypes = [testimonials]
+import brands from "./brands"
+import abouts from "./abouts"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+import contact from "./contact"
+export const schemaTypes = [testimonials, brands, abouts, experiences, skills, workExperience, works, contact]
+
