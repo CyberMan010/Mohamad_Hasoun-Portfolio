b45313209e12a75323bcf90652b3094b6dfb8de6
CyberMan010 <mohamahasoun60@gmail.com>
2025-04-27 17:38:01 +0300

    navbar set up all good

--- portfolio-front/src/App.tsx
+++ portfolio-front/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './App.scss'
 import { About, Testimonial, Footer, Header, Skills, Work } from './container'
+import { Navbar } from './components'
 
 function App() {
 
 
   return (
-    <div className="App">
+    <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
